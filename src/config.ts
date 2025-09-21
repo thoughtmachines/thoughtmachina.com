e9431ed2519287a78b618bcd44798ef1bd06ae9e
Jay Patel <jay1999ke@gmail.com>
2025-09-21 04:07:02 -0700

    about me

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "Thought",
+  name: "Thought.",
   title: "A postgres extension for semantic operators",
   description: "Thought Machines",
   accentColor: "#1d4ed8",
@@ -8,6 +8,5 @@ export const siteConfig = {
     github: "https://github.com/thoughtmachines",
   },
   aboutMe:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
-  skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker"],
+    "Supercharge your SQL with language model intelligence — semantic search, joins, aggregations, and more, all natively in Postgres. No app logic. Just smarter queries.",
 };
